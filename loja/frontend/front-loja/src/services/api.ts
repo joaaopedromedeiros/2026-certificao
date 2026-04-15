b809276d9d5f9cb3f710b6d49f92c3b712e5e597
joaaopedromedeiros <joaopedroaraujodemedeiros@gmail.com>
2026-04-15 14:57:56 -0300

    fix: endereço api backend correta

--- loja/frontend/front-loja/src/services/api.ts
+++ loja/frontend/front-loja/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://two026-certificao-3.onrender.com/api/docs/api",
+  baseURL: "https://two026-certificao-3.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
